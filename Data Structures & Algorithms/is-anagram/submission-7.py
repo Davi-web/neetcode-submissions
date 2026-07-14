a7ba3d5606b685920743ee9774980d5ee1a83beb
David Ha <65813362+Davi-web@users.noreply.github.com>
2026-07-14 15:04:19 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #1. sort both strings and check if they are equal. O(nlog(n))
+        # O(2 * nlog(n)) Time complexity for sorting both string, 
+        # return sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
